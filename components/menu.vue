2e4daafe3ba67444cd8ffdd1af373c7217427cee
harvanchik <66141166+harvanchik@users.noreply.github.com>
2022-09-03 12:48:54 -0700

    redesign to menu page; added music note in background [WIP]

--- components/menu.vue
+++ components/menu.vue
@@ -1,5 +1,47 @@
 <template>
-  <div class="text-align-center bg-red-50">
+  <!-- START: Background Music Notes -->
+  <div id="music-notes">
+    <object
+      data="../assets/img/croche-3.svg"
+      type="image/svg+xml"
+      width="10%"
+      class="absolute top-[10%] right-[10%] rotate-12 opacity-50"
+    ></object>
+  </div>
+  <!-- END: Background Music Notes -->
+
+  <!-- START: Body -->
+  <div class="flex flex-col pt-20">
+    <!-- START: Title -->
+    <h1 class="text-red-500 text-6xl mx-auto w-max">Songuessr</h1>
+    <!-- END: Title -->
+
+    <!-- START: Menu Buttons -->
+    <div class="flex flex-col space-y-5 mt-20 text-3xl mx-auto w-max">
+      <button
+        id="novice"
+        class="text-green-600 uppercase bg-green-200 rounded active:translate-y-1 hover:text-green-700 hover:bg-green-300 transition-all duration-250 hover:scale-105 mx-auto py-2 px-5 w-max"
+      >
+        Novice
+      </button>
+      <button
+        id="insane"
+        class="text-purple-600 uppercase bg-purple-200 rounded active:translate-y-1 hover:text-purple-700 hover:bg-purple-300 transition-[colors,transform] duration-250 hover:scale-105 mx-auto py-2 px-5 w-max"
+      >
+        Insane
+      </button>
+      <button
+        id="lightning"
+        class="text-blue-600 uppercase bg-blue-200 rounded active:translate-y-1 hover:text-blue-700 hover:bg-blue-300 transition-[colors,transform] duration-250 hover:scale-105 mx-auto py-2 px-5 w-max"
+      >
+        Lightning
+      </button>
+    </div>
+    <!-- END: Menu Buttons -->
+  </div>
+  <!-- END: Body -->
+
+  <!-- <div class="text-align-cente">
     <h1 class="text-red-400 text-5xl">Songuessr</h1>
     <br /><br />
     <p class="text-xl">The Song Clip Guessing Game</p>
@@ -10,5 +52,5 @@
     <br /><br />
     <button id="lightning" class="text-blue-400">Lightning</button>
     <br /><br />
-  </div>
+  </div> -->
 </template>
