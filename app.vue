6206765833be3e394952120d38797f19ba677b18
harvanchik <66141166+harvanchik@users.noreply.github.com>
2022-09-03 09:54:22 -0700

    created component for menu html moved it from the app.vue file

--- app.vue
+++ app.vue
@@ -1,14 +1,5 @@
 <template>
-  <div class="text-align-center bg-color-red-50">
-    <h1 class="text-red-400 text-5xl">Songuessr</h1>
-    <br><br>
-    <p class="text-xl">The Song Clip Guessing Game</p>
-    <br><br><br><br>
-    <button id="novice" class="text-green-600">Novice</button>
-    <br><br>
-    <button id="insane" class="text-purple-300">Insane</button>
-    <br><br>
-    <button id="lightning" class="text-blue-400">Lightning</button>
-    <br><br>
+  <div>
+    <Menu></Menu>
   </div>
 </template>
