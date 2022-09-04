2430e2e81162226e61704f1c3af97fe674202865
harvanchik <66141166+harvanchik@users.noreply.github.com>
2022-09-04 16:00:43 -0700

    removed commented html

--- components/menu.vue
+++ components/menu.vue
@@ -42,17 +42,4 @@
     <!-- END: Menu Buttons -->
   </div>
   <!-- END: Body -->
-
-  <!-- <div class="text-align-center">
-    <h1 class="text-red-400 text-5xl">Songuessr</h1>
-    <br /><br />
-    <p class="text-xl">The Song Clip Guessing Game</p>
-    <br /><br /><br /><br />
-    <button id="novice" class="text-green-600">Novice</button>
-    <br /><br />
-    <button id="insane" class="text-purple-300">Insane</button>
-    <br /><br />
-    <button id="lightning" class="text-blue-400">Lightning</button>
-    <br /><br />
-  </div> -->
 </template>
