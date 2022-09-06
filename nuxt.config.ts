dead9596d9adf4a2c4a513401e55d99452cd563b
Jake <66141166+harvanchik@users.noreply.github.com>
2022-09-06 13:37:39 -0700

    attempt at fixing css in head bug

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,5 @@
 import { defineNuxtConfig } from "nuxt";
+// import tailwindModule from "../src/module";
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
@@ -7,4 +8,12 @@ export default defineNuxtConfig({
   },
   modules: ["@nuxtjs/tailwindcss"],
   css: ["~/assets/css/tailwind.css"],
+  // modules: ["@nuxt/content", tailwindModule],
+  tailwindcss: {
+    exposeConfig: true,
+    injectPosition: "last",
+  },
+  content: {
+    documentDriven: true,
+  },
 });
