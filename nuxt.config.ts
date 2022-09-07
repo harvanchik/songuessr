e89b006c10a4285ec982b78805ee37885cbd6338
Jake <66141166+harvanchik@users.noreply.github.com>
2022-09-07 13:54:39 -0700

    added base path

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,9 @@ import { defineNuxtConfig } from "nuxt";
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  router: {
+    base: "./",
+  },
   typescript: {
     shim: false,
   },
