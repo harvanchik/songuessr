ca5464c221196a4c5078bc366739c34e796f362e
Ethan Stupin <ethstup@csu.fullerton.edu>
2022-09-08 14:42:57 -0700

    version 0.1.3

---
+++ components/genre.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h1 class="mx-auto text-red-500 text-7xl w-max">Songuessr</h1>
+        <div class="flex flex-row">
+            <img src="~/assets/img/70s.png" class="absolute top-[10%] right-[10%] rotate-12 opacity-50 z-10" />
+            <p>70's Pop</p>
+            <img src="" class="absolute top-[10%] right-[10%] rotate-12 opacity-50 z-10" />
+            <p>80's Pop</p>
+            <img src="" class="absolute top-[10%] right-[10%] rotate-12 opacity-50 z-10" />
+            <p>90's Pop</p>
+            <img src="" class="absolute top-[10%] right-[10%] rotate-12 opacity-50 z-10" />
+            <p>00's Pop</p>
+            <img src="" class="absolute top-[10%] right-[10%] rotate-12 opacity-50 z-10" />
+            <p>Country</p>
+            <img src="" class="absolute top-[10%] right-[10%] rotate-12 opacity-50 z-10" />
+            <p>Rock and Roll</p>
+            <img src="" class="absolute top-[10%] right-[10%] rotate-12 opacity-50 z-10" />
+            <p>Hip Hop</p>
+            <img src="" class="absolute top-[10%] right-[10%] rotate-12 opacity-50 z-10" />
+            <p>Alternative / Indie</p>
+        </div>
+    </div>
+</template>
