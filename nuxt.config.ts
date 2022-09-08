d1c9f5465da59a52a301435637cb4fe832b321f3
Jake <66141166+harvanchik@users.noreply.github.com>
2022-09-07 17:08:47 -0700

    removed router base pass

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,9 +3,6 @@ import { defineNuxtConfig } from "nuxt";
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  router: {
-    base: "./",
-  },
   typescript: {
     shim: false,
   },
