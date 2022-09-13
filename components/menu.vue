ce0ab336f3ed6689aff12bb2f146380f13bd1be4
Jake <66141166+harvanchik@users.noreply.github.com>
2022-09-13 12:47:37 -0700

    hide music note in background

--- components/menu.vue
+++ components/menu.vue
@@ -1,12 +1,12 @@
 <template>
   <!-- START: Background Music Notes -->
-  <div id="music-notes">
+  <div hidden id="music-notes">
     <!-- FIX: assets not being deployed for production -->
     <object
       data="~/assets/img/croche-3.svg"
       type="image/svg+xml"
       width="10%"
-      class="absolute top-[10%] right-[10%] rotate-12 opacity-50 z-10"
+      class="absolute top-[10%] right-[10%] z-10 rotate-12 opacity-50"
     ></object>
   </div>
   <!-- END: Background Music Notes -->
@@ -16,26 +16,26 @@
     class="flex flex-col h-screen pt-20 bg-gradient-to-br from-purple-100/80 via-red-100/80 to-orange-100/80"
   >
     <!-- START: Title -->
-    <h1 class="mx-auto text-red-500 text-7xl w-max">Songuessr</h1>
+    <h1 class="mx-auto text-6xl font-semibold text-red-500 w-max">Songuessr</h1>
     <!-- END: Title -->
 
     <!-- START: Menu Buttons -->
-    <div class="flex flex-col mx-auto mt-20 space-y-5 text-3xl w-max">
+    <div class="flex flex-col mx-auto mt-20 space-y-5 text-5xl w-max">
       <button
         id="novice"
-        class="px-5 py-2 mx-auto text-green-600 uppercase transition-all bg-green-200 rounded active:translate-y-1 hover:text-green-700 duration-250 hover:scale-105 w-max hover:bg-gradient-to-r hover:from-green-200 hover:to-green-400"
+        class="w-full px-5 py-2 mx-auto text-green-600 uppercase transition-all bg-green-200 rounded duration-250 hover:scale-105 hover:bg-gradient-to-r hover:from-green-200 hover:to-green-400 hover:text-green-700 active:translate-y-1"
       >
         Novice
       </button>
       <button
         id="insane"
-        class="text-purple-600 uppercase bg-purple-200 rounded active:translate-y-1 hover:text-purple-700 hover:bg-purple-300 transition-[colors,transform] duration-250 hover:scale-105 mx-auto py-2 px-5 w-max hover:bg-gradient-to-r hover:from-purple-200 hover:to-purple-400"
+        class="duration-250 mx-auto w-full rounded bg-purple-200 py-2 px-5 uppercase text-purple-600 transition-[colors,transform] hover:scale-105 hover:bg-purple-300 hover:bg-gradient-to-r hover:from-purple-200 hover:to-purple-400 hover:text-purple-700 active:translate-y-1"
       >
         Insane
       </button>
       <button
         id="lightning"
-        class="text-blue-600 uppercase bg-blue-200 rounded active:translate-y-1 hover:text-blue-700 hover:bg-blue-300 transition-[colors,transform] duration-250 hover:scale-105 mx-auto py-2 px-5 w-max hover:bg-gradient-to-r hover:from-blue-200 hover:to-blue-400"
+        class="duration-250 mx-auto w-full rounded bg-blue-200 py-2 px-5 uppercase text-blue-600 transition-[colors,transform] hover:scale-105 hover:bg-blue-300 hover:bg-gradient-to-r hover:from-blue-200 hover:to-blue-400 hover:text-blue-700 active:translate-y-1"
       >
         Lightning
       </button>
