d04433e3f9acd6f9b59ef9f8cb1823e0ef795e1d
Jake <66141166+harvanchik@users.noreply.github.com>
2022-09-13 13:04:35 -0700

    added pinia to project

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,10 @@
-import { defineNuxtConfig } from 'nuxt'
+import { defineNuxtConfig } from "nuxt";
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   typescript: {
     shim: false,
   },
-  target: 'static',
-  modules: ['@nuxtjs/tailwindcss', 'nuxt-icon'],
-})
+  target: "static",
+  modules: ["@nuxtjs/tailwindcss", "@pinia/nuxt", "nuxt-icon"],
+});
