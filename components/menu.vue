76a7f7d6db4a89f8280be778745e0e16084d4b67
Jake <66141166+harvanchik@users.noreply.github.com>
2022-09-15 13:41:42 -0700

    testing different button styles

--- components/menu.vue
+++ components/menu.vue
@@ -1,20 +1,6 @@
 <template>
-  <!-- START: Background Music Notes -->
-  <div hidden id="music-notes">
-    <!-- FIX: assets not being deployed for production -->
-    <object
-      data="~/assets/img/croche-3.svg"
-      type="image/svg+xml"
-      width="10%"
-      class="absolute top-[10%] right-[10%] z-10 rotate-12 opacity-50"
-    ></object>
-  </div>
-  <!-- END: Background Music Notes -->
-
   <!-- START: Body -->
-  <div
-    class="flex flex-col h-screen pt-20 bg-gradient-to-br from-purple-100/80 via-red-100/80 to-orange-100/80"
-  >
+  <div class="flex flex-col h-screen pt-20">
     <!-- START: Title -->
     <h1 class="mx-auto text-6xl font-semibold text-red-500 w-max">Songuessr</h1>
     <!-- END: Title -->
@@ -23,7 +9,7 @@
     <div class="flex flex-col mx-auto mt-20 space-y-5 text-5xl w-max">
       <button
         id="novice"
-        class="w-full px-5 py-2 mx-auto text-green-600 uppercase transition-all bg-green-200 rounded duration-250 hover:scale-105 hover:bg-gradient-to-r hover:from-green-200 hover:to-green-400 hover:text-green-700 active:translate-y-1"
+        class="w-full px-5 py-2 mx-auto text-black uppercase thing"
       >
         Novice
       </button>
