6dd7d708ef10f0b70cb175276cd39aeec26ba9dd
Ethan Stupin <ethstup@csu.fullerton.edu>
2022-09-15 14:43:37 -0700

    added functions in the buttons to change to genres page (still need coding for functions)

--- components/menu.vue
+++ components/menu.vue
@@ -8,18 +8,21 @@
     <!-- START: Menu Buttons -->
     <div class="flex flex-col mx-auto mt-20 space-y-5 text-5xl w-max">
       <button
+      @click="startNovice"
         id="novice"
         class="w-full px-5 py-2 mx-auto text-black uppercase thing"
       >
         Novice
       </button>
       <button
+      @click="startInter"
         id="insane"
         class="duration-250 mx-auto w-full rounded bg-purple-200 py-2 px-5 uppercase text-purple-600 transition-[colors,transform] hover:scale-105 hover:bg-purple-300 hover:bg-gradient-to-r hover:from-purple-200 hover:to-purple-400 hover:text-purple-700 active:translate-y-1"
       >
         Insane
       </button>
       <button
+      @click="startLight"
         id="lightning"
         class="duration-250 mx-auto w-full rounded bg-blue-200 py-2 px-5 uppercase text-blue-600 transition-[colors,transform] hover:scale-105 hover:bg-blue-300 hover:bg-gradient-to-r hover:from-blue-200 hover:to-blue-400 hover:text-blue-700 active:translate-y-1"
       >
