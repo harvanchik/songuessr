a5a6283c30666fee62399c3e9d2a55f522f24740
Ethan Stupin <estupin97@hotmail.com>
2022-09-21 12:55:59 -0700

    testing button functionality between webpages

--- components/menu.vue
+++ components/menu.vue
@@ -7,27 +7,31 @@
 
     <!-- START: Menu Buttons -->
     <div class="flex flex-col mx-auto mt-20 space-y-5 text-5xl w-max">
-      <button
-      @click="startNovice"
-        id="novice"
-        class="w-full px-5 py-2 mx-auto text-black uppercase thing"
-      >
-        Novice
-      </button>
-      <button
-      @click="startInter"
-        id="insane"
-        class="duration-250 mx-auto w-full rounded bg-purple-200 py-2 px-5 uppercase text-purple-600 transition-[colors,transform] hover:scale-105 hover:bg-purple-300 hover:bg-gradient-to-r hover:from-purple-200 hover:to-purple-400 hover:text-purple-700 active:translate-y-1"
-      >
-        Insane
-      </button>
-      <button
-      @click="startLight"
-        id="lightning"
-        class="duration-250 mx-auto w-full rounded bg-blue-200 py-2 px-5 uppercase text-blue-600 transition-[colors,transform] hover:scale-105 hover:bg-blue-300 hover:bg-gradient-to-r hover:from-blue-200 hover:to-blue-400 hover:text-blue-700 active:translate-y-1"
-      >
-        Lightning
-      </button>
+      <router-link :to="{ path: '/genre' }">
+        <button
+          id="novice"
+          class="w-full px-5 py-2 mx-auto text-black uppercase thing"
+        >
+          Novice
+        </button>
+      </router-link>
+      <router-link :to="{ path: '/genre' }">
+        <button
+          id="insane"
+          class="duration-250 mx-auto w-full rounded bg-purple-200 py-2 px-5 uppercase text-purple-600 transition-[colors,transform] hover:scale-105 hover:bg-purple-300 hover:bg-gradient-to-r hover:from-purple-200 hover:to-purple-400 hover:text-purple-700 active:translate-y-1"
+        >
+          Insane
+        </button>
+      </router-link>
+      <router-link :to="{ path: '/genre' }">
+        <button
+          id="lightning"
+          class="duration-250 mx-auto w-full rounded bg-blue-200 py-2 px-5 uppercase text-blue-600 transition-[colors,transform] hover:scale-105 hover:bg-blue-300 hover:bg-gradient-to-r hover:from-blue-200 hover:to-blue-400 hover:text-blue-700 active:translate-y-1"
+        >
+          Lightning
+        </button>
+      </router-link>
+      
     </div>
     <!-- END: Menu Buttons -->
   </div>
--- pages/genre.vue
+++ pages/genre.vue
@@ -3,29 +3,37 @@
         <h1 class="mx-auto text-red-500 text-7xl w-max">Songuessr</h1>
         <div class="flex flex-col mx-auto mt-20 space-y-5 text-5xl w-max">
             <h3 class="mx-auto text-5xl text-purple-400 w-max">Choose the Genre to Play</h3>
+            <br>
             <iframe src="https://open.spotify.com/embed/playlist/00KjsgI9t715OJFEUFziGJ" width="100%" height="80" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy">
             </iframe>
             <button id="70" class="duration-250 mx-auto w-full rounded bg-blue-200 py-2 px-5 
             uppercase text-blue-600 transition-[colors,transform] hover:scale-105 hover:bg-blue-300 hover:bg-gradient-to-r hover:from-blue-200 hover:to-blue-400 hover:text-blue-700 active:translate-y-1">70's Pop</button>
-            <iframe src="https://open.spotify.com/embed/playlist/37i9dQZF1DXb57FjYWz00c" width="100%" height="80" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+            <br>
+            <iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/37i9dQZF1DXb57FjYWz00c" width="100%" height="80" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
             <button id="80" class="duration-250 mx-auto w-full rounded bg-blue-200 py-2 px-5 
             uppercase text-blue-600 transition-[colors,transform] hover:scale-105 hover:bg-blue-300 hover:bg-gradient-to-r hover:from-blue-200 hover:to-blue-400 hover:text-blue-700 active:translate-y-1">80's Pop</button>
+            <br>
             <iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/37i9dQZF1EQn2GRFTFMl2A?utm_source=generator" width="100%" height="80" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
             <button id="90" class="duration-250 mx-auto w-full rounded bg-blue-200 py-2 px-5 
             uppercase text-blue-600 transition-[colors,transform] hover:scale-105 hover:bg-blue-300 hover:bg-gradient-to-r hover:from-blue-200 hover:to-blue-400 hover:text-blue-700 active:translate-y-1">90's Pop</button>
-            <iframe src="https://open.spotify.com/embed/playlist/37i9dQZF1DX4o1oenSJRJd" width="100%" height="80" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+            <br>
+            <iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/37i9dQZF1DX4o1oenSJRJd" width="100%" height="80" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
             <button id="00" class="duration-250 mx-auto w-full rounded bg-blue-200 py-2 px-5 
             uppercase text-blue-600 transition-[colors,transform] hover:scale-105 hover:bg-blue-300 hover:bg-gradient-to-r hover:from-blue-200 hover:to-blue-400 hover:text-blue-700 active:translate-y-1">00's Pop</button>
-            <iframe src="https://open.spotify.com/embed/playlist/37i9dQZF1DWYnwbYQ5HnZU" width="100%" height="80" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+            <br>
+            <iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/37i9dQZF1DWYnwbYQ5HnZU" width="100%" height="80" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
             <button id="country" class="duration-250 mx-auto w-full rounded bg-blue-200 py-2 px-5 
             uppercase text-blue-600 transition-[colors,transform] hover:scale-105 hover:bg-blue-300 hover:bg-gradient-to-r hover:from-blue-200 hover:to-blue-400 hover:text-blue-700 active:translate-y-1">Country</button>
-            <iframe src="https://open.spotify.com/embed/playlist/37i9dQZF1DXcF6B6QPhFDv" width="100%" height="80" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+            <br>
+            <iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/37i9dQZF1DXcF6B6QPhFDv" width="100%" height="80" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
             <button id="rnr" class="duration-250 mx-auto w-full rounded bg-blue-200 py-2 px-5 
             uppercase text-blue-600 transition-[colors,transform] hover:scale-105 hover:bg-blue-300 hover:bg-gradient-to-r hover:from-blue-200 hover:to-blue-400 hover:text-blue-700 active:translate-y-1">Rock and Roll</button>
-            <iframe src="https://open.spotify.com/embed/playlist/37i9dQZF1DX186v583rmzp" width="100%" height="80" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+            <br>
+            <iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/37i9dQZF1DX186v583rmzp" width="100%" height="80" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
             <button id="hh" class="duration-250 mx-auto w-full rounded bg-blue-200 py-2 px-5 
             uppercase text-blue-600 transition-[colors,transform] hover:scale-105 hover:bg-blue-300 hover:bg-gradient-to-r hover:from-blue-200 hover:to-blue-400 hover:text-blue-700 active:translate-y-1">Hip Hop</button>
-            <iframe src="https://open.spotify.com/embed/playlist/37i9dQZF1DX2Nc3B70tvx0" width="100%" height="80" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+            <br>
+            <iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/37i9dQZF1DX2Nc3B70tvx0" width="100%" height="80" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
             <button id="alt" class="duration-250 mx-auto w-full rounded bg-blue-200 py-2 px-5 
             uppercase text-blue-600 transition-[colors,transform] hover:scale-105 hover:bg-blue-300 hover:bg-gradient-to-r hover:from-blue-200 hover:to-blue-400 hover:text-blue-700 active:translate-y-1">Alternative / Indie</button>
             <br><br>
