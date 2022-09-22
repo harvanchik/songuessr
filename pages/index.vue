0c7227f9a91a278813264c5e97550c6064c42f55
Jake <66141166+harvanchik@users.noreply.github.com>
2022-09-22 14:12:07 -0700

    revert changes to use index.vue instead of app.vue

--- pages/index.vue
+++ pages/index.vue
@@ -4,25 +4,24 @@
   </div>
 </template>
 
-
 <script>
-  // TODO: use these functions to log the user's choice of game mode
-  export const state = () => ({
-    var1: null,
-    var2: null
-  })
+// TODO: use these functions to log the user's choice of game mode
+export const state = () => ({
+  var1: null,
+  var2: null,
+});
 
-  export const mutations = {
-    // Stores the difficulty for the game
-    SET_VAR_1 (state, value) {
-      console.log('SET_VAR_1', value)
-      state.var1 = value
-    },
-    // Stores the genre for the game
-    SET_VAR_2 (state, value) {
-      console.log('SET_VAR_2', value)
-      state.var2 = value
-    }
-  }
-  // END: log functions
+export const mutations = {
+  // Stores the difficulty for the game
+  SET_VAR_1(state, value) {
+    console.log("SET_VAR_1", value);
+    state.var1 = value;
+  },
+  // Stores the genre for the game
+  SET_VAR_2(state, value) {
+    console.log("SET_VAR_2", value);
+    state.var2 = value;
+  },
+};
+// END: log functions
 </script>
