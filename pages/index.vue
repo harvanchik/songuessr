4df59e181339b50c8c5cd4f682e7ee4f86cc5e79
Ethan Stupin <ethstup@csu.fullerton.edu>
2022-09-22 13:33:03 -0700

    edited javascript

--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,26 @@
+<template>
+  <div>
+    <Menu></Menu>
+  </div>
+</template>
+
+
 <script>
   // TODO: use these functions to log the user's choice of game mode
-  export default({
-    methods: {
-      logNovice () {
-        console.log('Clicked Novice')
-      },
-      logInsane () {
-        console.log('Clicked Insane')
-      },
-      logLightning () {
-        console.log('Clicked Lightning')
-      }
+  export const state = () => ({
+    var1: null,
+    var2: null
+  })
+
+  export const mutations = {
+    SET_VAR_1 (state, value) {
+      console.log('SET_VAR_1', value)
+      state.var1 = value
     },
-    mounted () {
-      this.$refs.Btn.click()
-    }})
+    SET_VAR_2 (state, value) {
+      console.log('SET_VAR_2', value)
+      state.var2 = value
+    }
+  }
   // END: log functions
-  </script>
-<template>
-    <div>
-      <Menu></Menu>
-    </div>
-  </template>
+</script>
