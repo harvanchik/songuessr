922f9b600699040f10198c63dc45938400297129
Jake <66141166+harvanchik@users.noreply.github.com>
2022-09-22 13:32:39 -0700

    change router-link to nuxt-link

--- components/menu.vue
+++ components/menu.vue
@@ -1,3 +1,20 @@
+<script>
+export default {
+  methods: {
+    logNovice() {
+      console.log("Clicked Novice");
+    },
+    logInsane() {
+      console.log("Clicked Insane");
+    },
+    logLightning() {
+      console.log("Clicked Lightning");
+    },
+  },
+};
+// END: log functions
+</script>
+
 <template>
   <!-- START: Body -->
   <div class="flex flex-col h-screen pt-20 bg-red-100">
@@ -7,34 +24,30 @@
 
     <!-- START: Menu Buttons -->
     <div class="flex flex-col mx-auto mt-20 space-y-5 text-5xl w-max">
-      <router-link :to="{ path: '/novice' }">
+      <nuxt-link to="/novice">
         <button
-        @click="logNovice"
           id="novice"
           class="duration-250 mx-auto w-full rounded bg-green-200 py-2 px-5 uppercase text-green-600 transition-[colors,transform] hover:scale-105 hover:bg-green-300 hover:bg-gradient-to-r hover:from-green-200 hover:to-green-400 hover:text-green-700 active:translate-y-1 thing"
         >
           Novice
         </button>
-      </router-link>
-      <router-link :to="{ path: '/insane' }">
+      </nuxt-link>
+      <nuxt-link to="/insane">
         <button
-          @click="logInsane"
           id="insane"
           class="duration-250 mx-auto w-full rounded bg-purple-200 py-2 px-5 uppercase text-purple-600 transition-[colors,transform] hover:scale-105 hover:bg-purple-300 hover:bg-gradient-to-r hover:from-purple-200 hover:to-purple-400 hover:text-purple-700 active:translate-y-1 thing"
         >
           Insane
         </button>
-      </router-link>
-      <router-link :to="{ path: '/lightning' }">
+      </nuxt-link>
+      <nuxt-link to="/lightning">
         <button
-          @click="logLightning"
           id="lightning"
           class="duration-250 mx-auto w-full rounded bg-blue-200 py-2 px-5 uppercase text-blue-600 transition-[colors,transform] hover:scale-105 hover:bg-blue-300 hover:bg-gradient-to-r hover:from-blue-200 hover:to-blue-400 hover:text-blue-700 active:translate-y-1 thing"
         >
           Lightning
         </button>
-      </router-link>
-      
+      </nuxt-link>
     </div>
     <!-- END: Menu Buttons -->
   </div>
